5f04d40b865e44f9727409a35c208c9529af8d2d
liziyu <liziyu0714@outlook.com>
2023-07-15 21:48:40 +0800

    添加版本信息读取工具:InfoChecker.cs

--- Tools/InfoChecker.cs
+++ Tools/InfoChecker.cs
@@ -2,9 +2,61 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace AnotherEWT.Tools
 {
-    
+    public class JsonInfo
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public string latest_version { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string condition { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string more_info { get; set; }
+
+    }
+
+    public class JsonInfoWrapper
+    {
+        public bool exception { get; set; }
+        public string error_message { get; set; }
+
+        public JsonInfo Info { get; set; }
+    }
+
+    public class InfoChecker
+    {
+        private string infoURL = "https://raw.githubusercontent.com/WTE-is-not-the-ewt/AnotherEWT/master/Update/Info.json";
+
+        public string InfoURL { get => infoURL;}
+
+        public async Task<JsonInfoWrapper> Check()
+        {
+            JsonInfoWrapper wrapper = new JsonInfoWrapper();
+            try
+            {
+                HttpClient client = new HttpClient();
+                string response = await client.GetStringAsync(infoURL);
+                if (response != null)
+                {
+                    JsonInfo info = JsonSerializer.Deserialize<JsonInfo> (response);
+                    wrapper.Info = info;
+                }
+            }
+            catch (Exception ex)
+            {
+                wrapper.exception = true;
+                wrapper.error_message = ex.Message;
+            }
+            return wrapper;
+        }
+    }
 }
