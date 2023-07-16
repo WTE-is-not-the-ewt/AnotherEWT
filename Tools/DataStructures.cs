6645d385c1b4b75eafe8f871658bf33afb586ced
liziyu <liziyu0714@outlook.com>
2023-07-16 22:29:39 +0800

    添加用于GetUser的实体类

---
+++ Tools/DataStructures.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnotherEWT.Tools
+{
+    public class StudentData
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public string userId { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string realName { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string userName { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string mobile { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public bool isLogin { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public int memberType { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string memberName { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public int now { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public bool isTemporaryExpire { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public bool isTemporaryMember { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public int temporaryExpireTime { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public int source { get; set; }
+    }
+
+    public class GetUserData
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public bool success { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string code { get; set; }
+        /// <summary>
+        /// 操作成功
+        /// </summary>
+        public string msg { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public StudentData data { get; set; }
+    }
+
+}
